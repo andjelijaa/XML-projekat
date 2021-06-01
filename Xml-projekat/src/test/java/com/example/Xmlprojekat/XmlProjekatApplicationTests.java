ef69e00d25c0b6f12563a78177e529913d3f2d31
andjelijaa <andjelija.djordjevic1997@gmail.com>
2021-06-01 17:58:33 +0200

    ...

---
+++ Xml-projekat/src/test/java/com/example/Xmlprojekat/XmlProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Xmlprojekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
