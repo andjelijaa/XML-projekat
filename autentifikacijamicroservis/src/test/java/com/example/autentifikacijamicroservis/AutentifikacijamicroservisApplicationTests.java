a03fe28cc938cca96cb4e51aaceea414a4f47297
andjelijaa <andjelija.djordjevic1997@gmail.com>
2021-06-02 18:06:10 +0200

    Kreiranje autentifikacije

---
+++ autentifikacijamicroservis/src/test/java/com/example/autentifikacijamicroservis/AutentifikacijamicroservisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autentifikacijamicroservis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutentifikacijamicroservisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
