89257bf7989063a7fd155a6eaeee2f302cd177e5
milanm98 <marjanovmilan13@gmail.com>
2021-06-13 16:49:51 +0200

    Nistagram landing page

---
+++ frontend/src/components/login/login.js
@@ -0,0 +1,7 @@
+function login(){
+    return(
+        <main></main>
+    )
+}
+
+export default login;
---
+++ frontend/src/components/register/register.js
@@ -0,0 +1,13 @@
+function registracija(){
+    return(
+        <main>
+            <form>
+                <div className="border border-blue-900 shadown-2xl p-10">
+                    <h1 className="font-mono font-bold text-2xl">Registracija</h1>
+                </div>
+             </form>
+        </main>
+    )
+}
+
+export default registracija;
