e33cdc32d137f22eb1bd46f5c82d1d563419acc1
milanm98 <marjanovmilan13@gmail.com>
2021-06-13 17:28:45 +0200

    login form done

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,6 +1,16 @@
 function login(){
     return(
-        <main></main>
+        <main className="flex w-screen h-screen justify-center items-center">
+            <div className="background w-screen h-screen"></div>
+            <form className="text-2xl font-bold flex flex-col border border-blue-900 shadown-2xl p-10 text-center">
+                    <h1 className="font-mono text-3xl mb-16">Login</h1>
+                    <label for="username">Korisnicko ime : </label>
+                    <input className="border border-12 shadow-2xl my-3" name="username" type="text"></input>
+                    <label for="password">Lozinka : </label>
+                    <input className="border border-12 shadow-2xl my-3" name="password" type="text"></input>    
+                    <button className="border border-12 border-white text-white my-12 text-3xl" type="submit">Potvrdi</button>
+             </form>
+        </main>
     )
 }
 
--- frontend/src/components/register/register.js
+++ frontend/src/components/register/register.js
@@ -6,12 +6,12 @@ function registracija(){
                     <h1 className="font-mono text-3xl mb-16">Registracija</h1>
                     <label for="username">Korisnicko ime : </label>
                     <input className="border border-12 shadow-2xl my-3" name="username" type="text"></input>
-                    <label for="username">Email : </label>
-                    <input className="border border-12 shadow-2xl my-3" name="username" type="text"></input>
-                    <label for="username">Lozinka : </label>
-                    <input className="border border-12 shadow-2xl my-3" name="username" type="text"></input>
-                    <label for="username">Ponovi lozinku : </label>
-                    <input className="border border-12 shadow-2xl my-3" name="username" type="text"></input>
+                    <label for="email">Email : </label>
+                    <input className="border border-12 shadow-2xl my-3" name="email" type="email"></input>
+                    <label for="password">Lozinka : </label>
+                    <input className="border border-12 shadow-2xl my-3" name="password" type="password"></input>
+                    <label for="repeat-password">Ponovi lozinku : </label>
+                    <input className="border border-12 shadow-2xl my-3" name="repeat-password" type="password"></input>
                     <button className="border border-12 border-white text-white my-12 text-3xl" type="submit">Potvrdi</button>
              </form>
         </main>
