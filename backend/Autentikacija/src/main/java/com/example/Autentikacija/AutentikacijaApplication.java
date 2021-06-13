54f2e3a18f97eba49fece0a05b0de41aa0541484
milanm98 <marjanovmilan13@gmail.com>
2021-06-13 19:24:51 +0200

    added auth folder

---
+++ backend/Autentikacija/src/main/java/com/example/Autentikacija/AutentikacijaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Autentikacija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutentikacijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutentikacijaApplication.class, args);
+	}
+
+}
