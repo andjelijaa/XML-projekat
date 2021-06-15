17f17f093abdbe45a486c9c12f85bd2f3e3ac9e0
andjelijaa <andjelija.djordjevic1997@gmail.com>
2021-06-15 13:32:08 +0200

    Resavanje baze i gresaka ,nedostatka anotacija @Service

--- backend/Autentikacija/src/main/java/com/example/Autentikacija/Security/UserDetailsServiceImpl.java
+++ backend/Autentikacija/src/main/java/com/example/Autentikacija/Security/UserDetailsServiceImpl.java
@@ -2,16 +2,22 @@ package com.example.Autentikacija.Security;
 
 import com.example.Autentikacija.Repository.ProfilRegistrovaniRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private ProfilRegistrovaniRepository profilRepo;
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return (UserDetails) profilRepo.findByUsername(username);
     }
+
 }
