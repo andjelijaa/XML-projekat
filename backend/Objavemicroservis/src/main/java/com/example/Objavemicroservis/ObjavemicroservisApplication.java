037bdede6caa51ded57a06787ef6a827aa70b2bd
andjelijaa <andjelija.djordjevic1997@gmail.com>
2021-06-21 00:29:32 +0200

    Sredjivanje dockera

--- backend/Objavemicroservis/src/main/java/com/example/Objavemicroservis/ObjavemicroservisApplication.java
+++ backend/Objavemicroservis/src/main/java/com/example/Objavemicroservis/ObjavemicroservisApplication.java
@@ -1,7 +1,13 @@
 package com.example.Objavemicroservis;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
 
 @SpringBootApplication
 public class ObjavemicroservisApplication {
