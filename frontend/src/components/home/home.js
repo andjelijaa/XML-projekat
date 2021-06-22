81800b296406d845b630b6ac3c1b9f3fd9184f6c
milanm98 <marjanovmilan13@gmail.com>
2021-06-22 12:37:34 +0200

    random posts on home page

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -13,18 +13,28 @@ function Home(){
         <main>
             <div className="background"></div>
             <nav className="flex flex-row justify-between items-center w-full text-center border border-12 p-4 border-blue-900 font-mono text-2xl bg-blue-400 text-white shadow-2xl">
-                    <Link to="/registracija" className="ml-12">
-                        Registracija
-                    </Link>
-                    <Link to="/" className="text-3xl leading-loose tracking-widest">
-                        Home
-                    </Link>
-                    <Link to="login" className="mr-12">
-                        Login
-                    </Link>
-                  
+                    <div>
+                        <Link to="/" className="ml-24">
+                            Home
+                        </Link>
+                        <Link className="mx-24">
+                            Pretrage
+                        </Link>
+                    </div>
+                 
+                    <div>
+                        <Link to="/registracija" className="mx-24">
+                            Registracija
+                        </Link>
+                        <Link to="/login" className="mr-24">
+                            Login
+                        </Link>
+                    </div>                  
             </nav>
             <div className=" flex flex-col justify-center items-center w-2/6 text-center margin">
+
+             
+
                 <div className="flex flex-col justify-center items-center border w-full border-12 border-blue-900 p-12 font-mono text-2xl text-blue-900 shadow-2xl bg-white my-12">
                     <h1 className="my-4">Bili Gejtic</h1>
                     <img className="post-pick my-4" src={biliGejtic} alt="bili gejtic"></img>
@@ -38,7 +48,7 @@ function Home(){
                 <div className="flex flex-col justify-center items-center border w-full border-12 border-blue-900 p-12 font-mono text-2xl text-blue-900 shadow-2xl bg-white my-12">
                     <h1 className="my-4">Marko Zukic</h1>
                     <img className="post-pick my-4" src={markoZukic} alt="bili gejtic"></img>
-                    <h3 className="my-4">#tb #oneyearago #vodka</h3>
+                    <h3 className="my-4">#tb #lastnight #prom</h3>
                     <div className="flex justify-between text-center w-2/6 my-4">
                         <i onClick={handleHeartClick} class="fa fa-heart"></i>
                         <i class="fa fa-comment"></i>
@@ -64,6 +74,8 @@ function Home(){
                         <i class="fa fa-comment"></i>
                     </div>
                 </div>
+
+               
             </div>
         </main>
     )
