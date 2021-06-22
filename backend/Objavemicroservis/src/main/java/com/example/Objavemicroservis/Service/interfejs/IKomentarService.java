c452a7a0809b2410bceb6bb310586109e07ad100
ivanaap98 <54770566+ivanaap98@users.noreply.github.com>
2021-06-23 01:21:30 +0200

    add service

---
+++ backend/Objavemicroservis/src/main/java/com/example/Objavemicroservis/Service/interfejs/IKomentarService.java
@@ -0,0 +1,6 @@
+package com.example.Objavemicroservis.service.interfejs;
+
+import com.mediamicroservice.mediamicroservice.controller.dto.CreateLikeDto;
+import com.mediamicroservice.mediamicroservice.domain.Like;
+
+import java.util.List;
