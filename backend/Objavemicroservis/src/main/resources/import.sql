eaf8f08d88e69b53dfb749e499c58612fb9947db
milanm98 <marjanovmilan13@gmail.com>
2021-06-29 18:18:59 +0200

    added feign client

--- backend/Objavemicroservis/src/main/resources/import.sql
+++ backend/Objavemicroservis/src/main/resources/import.sql
@@ -1,1 +1,3 @@
 insert into album(id) values(10);
+
+insert into objava (id, da_li_je_slika, komentari, lajkovano, naziv_fajla, opis, username, album_id, lokacija_id) VALUES (1, true, "dobar",false, "src","dobra slika","voja",2, 4);
