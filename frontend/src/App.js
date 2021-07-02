822cf0e98b2fdb1031e14b7510c77d47694df981
andjelijaa <andjelija.djordjevic1997@gmail.com>
2021-07-02 15:13:45 +0200

    Formiranje stranica na frontu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,11 @@ import Registracija from "./components/register/register";
 import Login from "./components/login/login";
 import Home from "./components/home/home";
 import Search from "./components/search/search";
+import Profil from './components/profil/profil';
+import Vidisvojeslike from './components/vidisvojeslike/vidisvojeslike';
+import Azuriranje from "./components/azuriranje/azuriranje";
+import Objavisliku from './components/objavisliku/objavisliku';
+
 
 function App() {
   return (
@@ -12,7 +17,12 @@ function App() {
         <Route component={Home} path="/" exact/>
         <Route component={Registracija} path="/registracija"/>
         <Route component={Login} path="/login"/>
-        <Route component={Search} path="/pretrage"></Route>
+        <Route component={Search} path="/pretrage"/>
+        <Route component={Profil} path="/profil"/>
+        <Route component={Vidisvojeslike} path="/vidisvojeslike"/>
+        <Route component={Azuriranje} path="/azuriranje"/>
+        <Route component={Objavisliku} path="/objavisliku">
+        </Route>
       </Switch>
     </BrowserRouter>
   );
