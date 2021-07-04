5bd536b9c98478fdddc28cfc1b7fa50ea68f91e7
ivanaap98 <54770566+ivanaap98@users.noreply.github.com>
2021-07-05 00:05:18 +0200

    dodate pretrage

--- backend/Objavemicroservis/src/main/java/com/example/Objavemicroservis/Service/ObjavaPodaciService.java
+++ backend/Objavemicroservis/src/main/java/com/example/Objavemicroservis/Service/ObjavaPodaciService.java
@@ -1,20 +1,31 @@
 package com.example.Objavemicroservis.Service;
 
 
+import com.example.Objavemicroservis.Connection.ProfilKonekcija;
 import com.example.Objavemicroservis.DTO.ObjavaDTO;
 import com.example.Objavemicroservis.DTO.SlikaDTO;
+import com.example.Objavemicroservis.Entity.Hashtag;
 import com.example.Objavemicroservis.Entity.Lokacija;
 import com.example.Objavemicroservis.Entity.Objava;
 import com.example.Objavemicroservis.Entity.ObjavaPodaci;
+import com.example.Objavemicroservis.ObjavaMapper;
 import com.example.Objavemicroservis.Repository.ObjavaPodaciRepository;
 import com.example.Objavemicroservis.Repository.ObjavaRepository;
 import com.example.Objavemicroservis.Service.interfejs.IHashtagService;
 import com.example.Objavemicroservis.Service.interfejs.ILokacijaService;
 import com.example.Objavemicroservis.Service.interfejs.IObjavaPodaciService;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ObjavaPodaciService implements IObjavaPodaciService {
@@ -31,10 +42,19 @@ public class ObjavaPodaciService implements IObjavaPodaciService {
     @Autowired
     private ObjavaRepository objavaRepository;
 
+    private static String upload = "profil-slike";
+
     @Override
     public List<ObjavaPodaci> getAll() {
         List<ObjavaPodaci> objavaPodacii = objavaPodaciRepository.findAll();
         return objavaPodacii;
+    }
+
+    private List<ObjavaPodaci> findAll() {
+        List<ObjavaPodaci> objavePodaci = objavaPodaciRepository.findAll();
+        if (objavePodaci.isEmpty())
+            return null;
+        return objavePodaci;
     }
 
     @Override
@@ -46,6 +66,30 @@ public class ObjavaPodaciService implements IObjavaPodaciService {
         return novaObjavaPodaci;
     }
 
+    @Override
+    public SlikaDTO slikaFile(ObjavaPodaci objavaPodaci, String filePath) {
+        SlikaDTO slikaDTO = ObjavaMapper.mapObjavaPodaciToSlikaDTO(objavaPodaci);
+        File in = new File(filePath + objavaPodaci.getObjava().getNazivFajla());
+        try {
+            slikaDTO.getKodSlike().add(IOUtils.toByteArray(new FileInputStream(in)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return slikaDTO;
+    }
+
+    @Override
+    public List<SlikaDTO> getSlikeFiles(List<ObjavaPodaci> objavePodaci) {
+        List<SlikaDTO> slikaDto = new ArrayList<>();
+        if (objavePodaci != null) {
+            String filePath = new File("").getAbsolutePath();
+            filePath = filePath.concat("/" + upload + "/");
+            for (ObjavaPodaci objavaPodaci : objavePodaci) {
+                slikaDto.add(slikaFile(objavaPodaci, filePath));
+            }
+        }
+        return slikaDto;
+    }
 
     @Override
     public ObjavaPodaci sacuvajSlikaInfo(ObjavaDTO slikaDto) {
@@ -63,30 +107,43 @@ public class ObjavaPodaciService implements IObjavaPodaciService {
     @Override
     public ObjavaPodaci getById(Long id) {
         ObjavaPodaci objavaPodaci = objavaPodaciRepository.findObjavaPodaciById(id);
-        if (objavaPodaci== null)
-            return null;
         return objavaPodaci;
 
     }
 
 
     @Override
-    public List<ObjavaPodaci> pretraziLokaciju(String lokacija) {
-        List<ObjavaPodaci> objavaPodacii = objavaPodaciRepository.findObjavaPodaciByLokacija(lokacija);
-        return objavaPodacii;
+    public List<SlikaDTO> pretraziLokaciju(String lokacija) {
+        List<ObjavaPodaci> objavePodaci = objavaPodaciRepository.findObjavaPodaciByLokacija(lokacija);
+        return getSlikeFiles(objavePodaci);
+    }
 
+    private List<String> getUsernamesByPost(List<ObjavaPodaci> objavePodaci) {
+        List<String> usernames = new ArrayList<>();
+        for (ObjavaPodaci objavaPodaci : objavePodaci) {
+            usernames.add(objavaPodaci.getObjava().getUsername());
+        }
+        Set<String> set = new HashSet<>(usernames);
+        usernames = set.stream().collect(Collectors.toList());
+        return usernames;
     }
 
     @Override
-    public List<ObjavaPodaci> pretraziTag(String tag) {
-        //TODO
-        return null;
+    public List<SlikaDTO> pretraziTag(String tag) {
+        List<ObjavaPodaci> sveObjavePodaci = findAll();
+        List<ObjavaPodaci> objavePodaci = new ArrayList<>();
+        for (ObjavaPodaci objavaPodaci : sveObjavePodaci) {
+            for (Hashtag hashtag : objavaPodaci.getObjava().getHashtagovi()) {
+                if (hashtag.getNazivhashtaga().toLowerCase().contains(tag.toLowerCase()))
+                    objavePodaci.add(objavaPodaci);
+            }
+        }
+//        List<String> publicProfiles = ProfilKonekcija.javniProfili(getUsernamesByPost(objavePodaci));
+        return getSlikeFiles(objavePodaci);
     }
 
     @Override
     public List<ObjavaPodaci> getJavneObjave() {
-//        List<ObjavaPodaci> objavaPodacii = findAll();
-//        return filterPublicPostByUsernames(usernames, posts);
         //TODO
         return null;
     }
