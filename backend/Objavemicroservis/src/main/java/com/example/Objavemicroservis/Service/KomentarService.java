53866c9d24d2503b6377a908d97b2a312831f98f
andjelijaa <andjelija.djordjevic1997@gmail.com>
2021-07-08 21:04:15 +0200

    Komenari dodavanje

--- backend/Objavemicroservis/src/main/java/com/example/Objavemicroservis/Service/KomentarService.java
+++ backend/Objavemicroservis/src/main/java/com/example/Objavemicroservis/Service/KomentarService.java
@@ -3,6 +3,7 @@ package com.example.Objavemicroservis.Service;
 
 import com.example.Objavemicroservis.DTO.KomentarDTO;
 import com.example.Objavemicroservis.Entity.Komentar;
+import com.example.Objavemicroservis.Entity.ObjavaPodaci;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.Objavemicroservis.Service.interfejs.IKomentarService;
@@ -15,6 +16,9 @@ public class KomentarService implements IKomentarService {
 
     @Autowired
      private KomentarRepository komentarRepository;
+
+    @Autowired
+    private  ObjavaPodaciService objavaPodaciService;
 
     @Override
     public List<Komentar> getAllByPost(Long id) {
@@ -29,7 +33,12 @@ public class KomentarService implements IKomentarService {
         komentar.setUsername(komentarDTO.getUsername());
         komentar.setTekst(komentarDTO.getTekst());
         komentarRepository.save(komentar);
-     //dodati
+        ObjavaPodaci objavaPodaci= objavaPodaciService.getById(komentarDTO.getObjavaID());
+        List<Komentar> komentars=objavaPodaci.getKomentari();
+        komentars.add(komentar);
+
+        komentarRepository.save(komentar);
+
 
         return komentar;
     }
